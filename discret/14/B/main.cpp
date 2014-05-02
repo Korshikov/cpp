4b55a5b69a31ada57a892f8e382ba5b67b5181ac
korshikov <pk.95@mail.ru>
2014-05-03 00:55:45 +0400

    try do C

---
+++ discret/14/B/main.cpp
@@ -0,0 +1,119 @@
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long int SegmentTreeNodeValueType;
+const SegmentTreeNodeValueType netralElement = 0;
+
+
+struct SegmentTreeNode {
+    //long long left,right;
+    SegmentTreeNodeValueType value;
+
+    SegmentTreeNode() {
+        value = netralElement;
+    }
+};
+
+struct SegmentTree {
+    SegmentTree(SegmentTreeNodeValueType *a, const long long &n) {
+        treeN = 1 << (SegmentTreeNodeValueType) ceil(log2(n));
+        treeLength = treeN << 1;
+        tree = new SegmentTreeNode[treeLength];
+        for (int i = treeN - 1; i < n + treeN - 1; i++) {
+            tree[i].value = a[i - treeN + 1];
+        }
+        for (int i = treeN - 2; i >= 0; i--) {
+            tree[i].value = functionInNode(tree[i * 2 + 1].value, tree[i * 2 + 2].value);
+        }
+    }
+
+    ~SegmentTree() {
+        delete  []tree;
+    }
+
+    SegmentTreeNodeValueType request(long long left, long long right) {
+        return request(left, right, 0, 0, treeN - 1);
+    }
+
+    void modifideList(long long elementNumber, SegmentTreeNodeValueType newValue) {
+        tree[treeN + elementNumber - 1].value = newValue;
+        updateParentValue(treeN + elementNumber - 1);
+    }
+
+private:
+    SegmentTreeNode *tree;
+    long long int treeLength, treeN;
+
+    SegmentTreeNodeValueType functionInNode(SegmentTreeNodeValueType left, SegmentTreeNodeValueType right) {
+        return left+ right;
+    }
+
+    SegmentTreeNodeValueType request(long long left, long long right, long long nodeNumber, SegmentTreeNodeValueType tLeft, SegmentTreeNodeValueType tRight) {
+        if (left > right) {
+            return netralElement;
+        }
+        if ((left == tLeft) && (right == tRight)) {
+            return tree[nodeNumber].value;
+        }
+        long long tMidle = tLeft + ((tRight - tLeft) / 2);
+        return functionInNode(
+                request(left, min(right, tMidle), nodeNumber * 2 + 1, tLeft, tMidle),
+                request(max(left, tMidle + 1), right, nodeNumber * 2 + 2, tMidle + 1, tRight)
+        );
+    }
+
+    void updateParentValue(long long nodeNumber) {
+        if (nodeNumber == 0) {
+            return;
+        }
+        long long parentNumber = (nodeNumber - 1) / 2;
+        SegmentTreeNodeValueType newValue = functionInNode(tree[parentNumber * 2 + 1].value, tree[parentNumber * 2 + 2].value);
+        if (tree[parentNumber].value != newValue) {
+            tree[parentNumber].value = newValue;
+            updateParentValue(parentNumber);
+        }
+    }
+
+};
+
+int main() {
+    freopen("rsq.in","r",stdin);
+    freopen("rsq.out","w+",stdout);
+    long long int n;
+    SegmentTreeNodeValueType *a;
+
+    scanf("%lld", &n);
+    a = new SegmentTreeNodeValueType[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &a[i]);
+    }
+
+    SegmentTree tree(a, n);
+
+    char c;
+    long long b1,b2;
+
+    while ((scanf("%c",&c)!= EOF)&&(c!= EOF)){
+        if(c=='\n'){
+            continue;
+        }
+        switch(c){
+            case 's':
+                if((scanf("%c",&c)=='e')||(c=='e')){
+                    scanf("t %lld %lld\n",&b1,&b2);
+                    tree.modifideList(b1-1, b2);
+                }else{
+                    scanf("m %lld %lld\n",&b1,&b2);
+                    printf("%lld\n", tree.request(b1-1,b2-1));
+                }
+                break;
+        }
+    }
+
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
---
+++ discret/14/C/main.cpp
@@ -0,0 +1,163 @@
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+long long r;
+
+struct SegmentTreeNodeValueType {
+    long long matrix[2][2];
+
+
+    SegmentTreeNodeValueType() {
+
+    }
+    //with out constructor;
+    SegmentTreeNodeValueType(int){
+        matrix[0][0]=1;
+        matrix[0][1]=0;
+        matrix[1][0]=0;
+        matrix[1][1]=1;
+    }
+
+    const SegmentTreeNodeValueType operator*(SegmentTreeNodeValueType const& data);
+
+    SegmentTreeNodeValueType operator= (SegmentTreeNodeValueType const& data);
+
+    bool operator!= (SegmentTreeNodeValueType const&data) {
+        return ((this->matrix[0][0]!= data.matrix[0][0])||
+                (this->matrix[0][1]!= data.matrix[0][1])||
+                (this->matrix[1][0]!= data.matrix[1][0])||
+                (this->matrix[1][1]!= data.matrix[1][1]));
+    }
+
+
+};
+
+SegmentTreeNodeValueType SegmentTreeNodeValueType::operator = (SegmentTreeNodeValueType const& data) {
+    this->matrix[0][0] = data.matrix[0][0];
+    this->matrix[0][1] = data.matrix[0][1];
+    this->matrix[1][0] = data.matrix[1][0];
+    this->matrix[1][1] = data.matrix[1][1];
+    return *this;
+}
+
+const SegmentTreeNodeValueType SegmentTreeNodeValueType::operator *(SegmentTreeNodeValueType const &data) {
+    SegmentTreeNodeValueType result;
+    result.matrix[0][0] = (this->matrix[0][0] * data.matrix[0][0] + this->matrix[0][1] * data.matrix[1][0]) % r;
+    result.matrix[0][1] = (this->matrix[0][0] * data.matrix[0][1] + this->matrix[0][1] * data.matrix[1][1]) % r;
+    result.matrix[1][0] = (this->matrix[1][0] * data.matrix[0][0] + this->matrix[1][1] * data.matrix[1][0]) % r;
+    result.matrix[1][1] = (this->matrix[1][0] * data.matrix[0][1] + this->matrix[1][1] * data.matrix[1][1]) % r;
+
+    return result;
+}
+
+
+
+
+//typedef long long int SegmentTreeNodeValueType;
+SegmentTreeNodeValueType const neutralElement(0);
+
+struct SegmentTreeNode {
+    //long long left,right;
+    SegmentTreeNodeValueType value;
+
+    SegmentTreeNode(){
+
+    }
+};
+
+struct SegmentTree {
+    SegmentTree(SegmentTreeNodeValueType *a, const long long &n) {
+        treeN = 1 << (long long) ceil(log2(n));
+        treeLength = treeN << 1;
+        tree = new SegmentTreeNode[treeLength];
+        for (int i = treeN - 1; i < n + treeN - 1; i++) {
+            tree[i].value = a[i - treeN + 1];
+        }
+        for(int i= n + treeN - 1; i< treeLength;i++){
+            tree[i].value = neutralElement;
+        }
+        for (int i = treeN - 2; i >= 0; i--) {
+            tree[i].value = functionInNode(tree[i * 2 + 1].value, tree[i * 2 + 2].value);
+        }
+    }
+
+    ~SegmentTree() {
+        delete  []tree;
+    }
+
+    SegmentTreeNodeValueType request(long long left, long long right) {
+        return request(left, right, 0, 0, treeN - 1);
+    }
+
+    void modifideList(long long elementNumber, SegmentTreeNodeValueType newValue) {
+        tree[treeN + elementNumber - 1].value = newValue;
+        updateParentValue(treeN + elementNumber - 1);
+    }
+
+private:
+    SegmentTreeNode *tree;
+    long long int treeLength, treeN;
+
+    SegmentTreeNodeValueType functionInNode(SegmentTreeNodeValueType left,const SegmentTreeNodeValueType right) {
+        return left*right;
+    }
+
+    SegmentTreeNodeValueType request(long long left, long long right, long long nodeNumber, long long tLeft, long long tRight) {
+        if (left > right) {
+            return neutralElement;
+        }
+        if ((left == tLeft) && (right == tRight)) {
+            return tree[nodeNumber].value;
+        }
+        long long tMidle = tLeft + ((tRight - tLeft) / 2);
+        return functionInNode(
+                request(left, min(right, tMidle), nodeNumber * 2 + 1, tLeft, tMidle),
+                request(max(left, tMidle + 1), right, nodeNumber * 2 + 2, tMidle + 1, tRight)
+        );
+    }
+
+    void updateParentValue(long long nodeNumber) {
+        if (nodeNumber == 0) {
+            return;
+        }
+        long long parentNumber = (nodeNumber - 1) / 2;
+        SegmentTreeNodeValueType newValue = functionInNode(tree[parentNumber * 2 + 1].value, tree[parentNumber * 2 + 2].value);
+        if (tree[parentNumber].value != newValue) {
+            tree[parentNumber].value = newValue;
+            updateParentValue(parentNumber);
+        }
+    }
+
+};
+
+int main() {
+    freopen("crypto.in","r",stdin);
+    freopen("crypto.out","w+",stdout);
+    long long int n,m;
+    SegmentTreeNodeValueType *a;
+
+    scanf("%lld %lld %lld\n",&r, &n,&m);
+    a = new SegmentTreeNodeValueType[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%lld %lld\n%lld %lld\n", &a[i].matrix[0][0],&a[i].matrix[0][1],&a[i].matrix[1][0],&a[i].matrix[1][1]);
+    }
+
+    SegmentTree tree(a, n);
+
+    long long b1, b2;
+    SegmentTreeNodeValueType result(0);
+
+    for(int i=0;i<m;i++){
+        scanf("%lld %lld\n",&b1,&b2);
+        result = tree.request(b1-1, b2-1);
+        printf("%lld %lld\n%lld %lld\n\n",result.matrix[0][0],result.matrix[0][1],result.matrix[1][0],result.matrix[1][1]);
+    }
+
+
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
