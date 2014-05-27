e874f04cb6080b8ea2b8bc9a818a7d5c6ac2f84c
korshikov <pk.95@mail.ru>
2014-05-27 20:12:20 +0400

    F

---
+++ discret/16/F/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    freopen("antiqs.in","r",stdin);
+    freopen("antiqs.out","w+",stdout);
+    int n;
+    scanf("%d",&n);
+    int *a = new int[n+1];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=i+1;
+    }
+    for(int i=2;i<n;i++)
+    {
+        std::swap(a[i],a[i/2]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    delete[] a;
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
